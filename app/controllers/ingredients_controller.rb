9f1e96a93fbeb8083d5e4f998ecd8a455eaa66f2
teasacura <teasacuraster@gmail.com>
2018-05-28 20:47:28 -0400

    Done.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -42,6 +42,6 @@ class IngredientsController < ApplicationController
   private
 
   def ingredient_params
-    params.require(:ingredient).permit(:name)
+    params.require(:ingredient).permit(:name, :quantity)
   end
 end
