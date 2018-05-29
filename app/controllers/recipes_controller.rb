0a6cad470c55f030ceb81bfc98b5e2c056b2bf3e
teasacura <teasacuraster@gmail.com>
2018-05-28 20:34:18 -0400

    Done.

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,43 @@
+require "pry"
+
+class RecipesController < ApplicationController
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.valid?
+      @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    if @recipe.valid?
+      @recipe.save
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, ingredient_ids: [])
+  end
+end
